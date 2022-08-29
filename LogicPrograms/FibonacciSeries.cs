52425c418e03f10e0a3fd66ae39af6a201432d86
AnjaliRSahu <anjalisahu1996@gmail.com>
2022-08-29 18:56:10 +0530

    [Anjali Sahu] added Fibonacci series file

---
+++ LogicPrograms/FibonacciSeries.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicPrograms
+{
+    internal class FibonacciSeries
+    {
+
+        public static void Fibonacci()
+        {
+            Console.WriteLine("Enter a number:");
+            int a = Convert.ToInt32(Console.ReadLine());
+
+            int num1 = 0, num2 = 1, num3;
+            if (a == 0)
+                Console.WriteLine(0);
+            if (a == 1)
+                Console.WriteLine(1);
+            else
+                Console.WriteLine("Fibonacci Series of first {0} numbers are", a);
+            {
+                Console.WriteLine();
+                for (int i = 1; i <= a; i++)
+                {
+                    num3 = num1 + num2;
+                    Console.WriteLine(num3 + "");
+                    num1 = num2;
+                    num2 = num3;
+                }
+            }
+
+        }
+
+    }
+}
---
+++ LogicPrograms/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace LogicPrograms
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Logic Programs");
+            Console.WriteLine("Choose options \n 1.Fibonacci Series ");
+            int option = Convert.ToInt32(Console.ReadLine());
+
+            switch (option)
+            {
+                case 1:
+                    FibonacciSeries.Fibonacci();
+                    break;
+
+                default:
+                    Console.WriteLine("Wrong Choice");
+                    break;
+            }
+
+        }
+    }
+}
